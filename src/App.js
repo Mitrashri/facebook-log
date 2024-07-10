21faf424f64a01bfd2345e5fd6e44600279cb37f
Mitrashri <mithugeethupaaps@gmail.com>
2024-07-10 09:13:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './components/Fb';
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Facebook Authentication
+      </p>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Fb.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+
+export default class Login extends Component {
+    state = {
+        auth : false,
+        name: '',
+        picture:'',
+        pages: []
+    }
+    componentClicked = ()=>{
+        console.log("button clicked");
+    }
+    responseFacebook = (response) => {
+        console.log(response);
+        if(response.status!=='unknown'){
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url,
+        });
+        this.fetchPages(response.accessToken);
+        } else{
+            console.log('Facebook login failed')
+        }
+    };
+
+    fetchPages = (accessToken) =>{
+        fetch(`https://graph.facebook.com/v2.11/me/accounts?access_token=${accessToken}`) 
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            this.setState({ pages: data.data});
+          })
+    }
+    render(){
+        let fbData;
+
+        this.state.auth ? 
+           fbData = (
+            <div>
+                <img src={this.state.picture} alt={this.state.name} />
+                <h2>Welcome, {this.state.name}</h2>
+            </div>
+           ):
+           fbData = (
+            <FacebookLogin
+            appId="856902982968624"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook} 
+            scope="pages_show_list"/>
+           );
+
+
+        return(
+           <>
+             {fbData}
+           </>
+        );
+    }
+}
